a2d4d21fc6d2a52bd045db95c7579e51bb5f68dd
thomas01155 <thomas01155@gmail.com>
2015-09-12 20:26:24 +0100

    fixes and hash links

--- js/background-parallax.js
+++ js/background-parallax.js
@@ -6,8 +6,8 @@ function setup() {
 
     $('#jsParallaxBackground').height(height + 300);
 
-    if(navigator.userAgent.match(/Trident\/7\./) || /Edge\/12./i.test(navigator.userAgent) || (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0)) { // if IE
-        $('body').on("mousewheel", function () {
+    if (navigator.userAgent.match(/Trident\/7\./) || /Edge\/12./i.test(navigator.userAgent) || (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0)) { // if IE
+        $('body').on("mousewheel", function() {
             // remove default behavior
             event.preventDefault();
 
@@ -17,6 +17,19 @@ function setup() {
             window.scrollTo(0, currentScrollPosition - wheelDelta);
         });
     }
+
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
 }
 
 module.exports = setup
